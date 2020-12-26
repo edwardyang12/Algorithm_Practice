a06931b0f776b8cf8337e051ec4785a89495cef3
edwardyang12 <edwardyang016@gmail.com>
2020-12-25 21:32:56 -0800

    another problem

---
+++ BST/285.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    // sucessor is the next largest element
+    // successor is always to the right of p
+    // case 1: no right child -> go up until you reach first element greater than p 
+    // case 2: right child -> go right then all the way left
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode temp = root;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(null);
+        while(temp!=p){
+            stack.push(temp);
+            if(p.val>temp.val){
+                temp = temp.right;
+            }
+            else{
+                temp = temp.left;
+            }
+        }
+        while(temp!=null && temp.val<=p.val){
+            if(temp.right!=null){
+                temp = temp.right;
+                while(temp.left!=null){
+                    temp = temp.left;
+                }
+            }
+            else{
+                temp = stack.pop();
+            }
+
+        }
+        return temp;
+    }
+}
