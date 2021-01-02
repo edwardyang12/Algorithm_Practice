bf82930e4a8073178546e8a420d93872889826c1
edwardyang12 <edwardyang016@gmail.com>
2021-01-01 22:26:30 -0800

    problem

---
+++ DP/300.java
@@ -0,0 +1,27 @@
+class Solution {
+
+
+    // dp array stores max LIS for subsequences up to that index
+    // for each additional index, will find max value from prior array where nums[new] > nums[index]
+    // O(n^2) time
+    // O(n) space
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < dp.length; i++) {
+            int maxval = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    maxval = Math.max(maxval, dp[j]);
+                }
+            }
+            dp[i] = maxval + 1;
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+}
